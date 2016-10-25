ea8b62e47bdd1a30248bb68bd4be700c701d5685
Greg Steuck <blackgnezdo@gmail.com>
2016-10-25 12:43:06 -0700

    Haddock (#3)
    * Trivial script for regenerating haddocks: exclude .haddock files.
    
    * Haddock regen

--- tools/haddock.sh
+++ tools/haddock.sh
@@ -10,8 +10,9 @@ STACK="stack --docker --docker-image=$IMAGE_NAME"
 $STACK haddock --no-haddock-deps tensorflow*
 DOC_ROOT=$($STACK path --local-doc-root)
 DOCS=docs/haddock
-git rm -r $DOCS
+git rm -fr $DOCS
 mkdir -p $DOCS
 cp $DOC_ROOT/{*.html,*js,*.png,*.gif,*.css} $DOCS
 cp -a $DOC_ROOT/tensorflow* $DOCS
+rm -f $DOCS/*/*.haddock
 git add $DOCS
