db69e1d311af54e0f38cf8fa2ca1b53fdab22b1b
Greg Steuck <blackgnezdo@gmail.com>
2016-10-25 10:11:24 -0700

    Trivial script for regenerating haddocks (#2)

---
+++ tools/haddock.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Creates shallow haddocks for GitHub pages.
+
+set -eu -o pipefail
+
+IMAGE_NAME=tensorflow/haskell:v0
+STACK="stack --docker --docker-image=$IMAGE_NAME"
+
+$STACK haddock --no-haddock-deps tensorflow*
+DOC_ROOT=$($STACK path --local-doc-root)
+DOCS=docs/haddock
+git rm -r $DOCS
+mkdir -p $DOCS
+cp $DOC_ROOT/{*.html,*js,*.png,*.gif,*.css} $DOCS
+cp -a $DOC_ROOT/tensorflow* $DOCS
+git add $DOCS
