7db489f27efb96dc0f9b276b9468f7d897b0e0af
Greg Steuck <gnezdo@google.com>
2017-01-01 02:35:57 -0800

    No need to docker pull now

--- ci_build/outer_launch_tests.sh
+++ ci_build/outer_launch_tests.sh
@@ -8,6 +8,5 @@ STACK_RESOLVER=${STACK_RESOLVER:-lts-6.2}
 IMAGE_NAME=tensorflow/haskell/ci_build:$STACK_RESOLVER
 
 git submodule update
-docker pull tensorflow/tensorflow:nightly-devel
 docker build --build-arg STACK_RESOLVER=$STACK_RESOLVER -t $IMAGE_NAME -f ci_build/Dockerfile .
 docker run $IMAGE_NAME stack build --resolver=$STACK_RESOLVER --pedantic --test
