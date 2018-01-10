daad5fc5f7905b9fbab8d08d487ac12027687c0e
fkm3 <frederickmayle@gmail.com>
2018-01-10 18:56:06 -0500

    Update outer_launch_tests.sh

--- ci_build/outer_launch_tests.sh
+++ ci_build/outer_launch_tests.sh
@@ -6,6 +6,5 @@ set -eu -o pipefail
 
 IMAGE_NAME=tensorflow/haskell/ci_build:lts8
 
-git submodule update
 docker build -t $IMAGE_NAME -f ci_build/Dockerfile .
 docker run $IMAGE_NAME stack build --pedantic --test
