bc15709cb4bb4b41d232392bc4db6ca610a1234d
fkm3 <frederickmayle@gmail.com>
2018-01-16 16:16:41 -0500

    Update outer_launch_tests.sh to support new internal CI (#173)

--- ci_build/outer_launch_tests.sh
+++ ci_build/outer_launch_tests.sh
@@ -2,10 +2,13 @@
 
 # Builds a test image and runs the tests inside.
 
-set -eu -o pipefail
+set -eux -o pipefail
 
 IMAGE_NAME=tensorflow/haskell/ci_build:lts8
 
-git submodule update
+# Make sure we are in the root directory of the repositiory.
+cd "$( dirname "$0" )"/..
+
+git submodule update --init --recursive
 docker build -t $IMAGE_NAME -f ci_build/Dockerfile .
 docker run $IMAGE_NAME stack build --pedantic --test
